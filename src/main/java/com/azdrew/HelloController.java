ad5185277377a00b1923a13d86cc4410911fe95a
Andrew Sandstrom <andrew.sandstrom@fisglobal.com>
2017-04-22 18:00:14 -0700

    logging/actuator

--- src/main/java/com/azdrew/HelloController.java
+++ src/main/java/com/azdrew/HelloController.java
@@ -1,14 +1,19 @@
 package com.azdrew;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-	
-	  @RequestMapping("hello")
-	    public String sayHello(){
-	        return ("Hello, SpringBoot on Wildfly");
-	    }
+
+	private static final Logger LOG = LoggerFactory.getLogger(HelloController.class);
+
+	@RequestMapping("hello")
+	public String sayHello() {
+		LOG.info("Here we are");
+		return ("Hello, SpringBoot on Wildfly");
+	}
 
 }
